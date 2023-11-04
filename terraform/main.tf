e1cad2dd4fa3a3268cf43fd57fffbd89b5acf133
Rafael de Miranda <37124918+rafaeldemiranda95@users.noreply.github.com>
2023-11-04 17:43:15 -0300

    Teste

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,28 @@
+provider "google" {
+  project = "tech-challenge"
+  region  = "us-west1"            
+}
 resource "google_compute_network" "vpc" {
-  name                    = "my-vpc"
+  name                    = "tech-challenge-vpc"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnetwork" {
-  name          = "my-subnet"
+  name          = "tech-challenge-subnetwork"
   ip_cidr_range = "10.0.1.0/24"
   region        = "us-west1"
   network       = google_compute_network.vpc.id
 }
 
 resource "google_container_cluster" "cluster" {
-  name     = "my-gke-cluster"
+  name     = "tech-challenge-cluster"
   location = "us-west1"
 
   remove_default_node_pool = true
   initial_node_count       = 1
 
   node_pool {
-    name       = "my-node-pool"
+    name       = "tech-challenge-node-pool"
     node_count = 1
 
     node_config {
@@ -32,3 +36,5 @@ resource "google_container_cluster" "cluster" {
     }
   }
 }
+
+
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
-  description = "The ID of the VPC"
+  description = "O ID da VPC"
   value       = google_compute_network.vpc.id
 }
 
 output "subnet_ids" {
-  description = "The IDs of the subnets"
+  description = "Os IDs das sub-redes"
   value       = google_compute_subnetwork.subnetwork.id
 }
 
 output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
+  description = "Ponto de extremidade para o plano de controle do GKE."
   value       = google_container_cluster.cluster.endpoint
 }
 
 output "cluster_ca_certificate" {
-  description = "The cluster ca certificate (base64 encoded)"
+  description = "O certificado ca do cluster (codificado em base64)"
   value       = google_container_cluster.cluster.master_auth.0.cluster_ca_certificate
 }
