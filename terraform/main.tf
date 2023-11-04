cd46c17dc715a48b57186a9cb07459b580585460
Rafael de Miranda <37124918+rafaeldemiranda95@users.noreply.github.com>
2023-11-04 18:01:00 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 provider "google" {
   project = "tech-challenge"
-  region  = "us-west1"            
+  region  = "us-west1"
 }
 resource "google_compute_network" "vpc" {
   name                    = "tech-challenge-vpc"
@@ -10,13 +10,13 @@ resource "google_compute_network" "vpc" {
 resource "google_compute_subnetwork" "subnetwork" {
   name          = "tech-challenge-subnetwork"
   ip_cidr_range = "10.0.1.0/24"
-  region        = "us-west1"
+  region        = "us-central1"
   network       = google_compute_network.vpc.id
 }
 
 resource "google_container_cluster" "cluster" {
   name     = "tech-challenge-cluster"
-  location = "us-west1"
+  location = "us-central1"
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -26,7 +26,7 @@ resource "google_container_cluster" "cluster" {
     node_count = 1
 
     node_config {
-      machine_type = "e2-medium"
+      machine_type = "g1-small"
       oauth_scopes = [
         "https://www.googleapis.com/auth/compute",
         "https://www.googleapis.com/auth/devstorage.read_only",
