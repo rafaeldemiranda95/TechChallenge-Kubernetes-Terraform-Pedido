a0f5be9f10c7079790d69706b71d732efb2e98e1
Rafael de Miranda <37124918+rafaeldemiranda95@users.noreply.github.com>
2024-01-29 00:07:16 -0300

    Teste

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  project = "tech-challenge"
-  region  = "us-west1"
+  project = "tech-challenge" # Substitua pelo seu ID de projeto do GCP
+  region  = "us-west1"       # Região onde o Terraform criará os recursos, se necessário
 }
 
 resource "google_compute_network" "vpc" {
@@ -11,13 +11,13 @@ resource "google_compute_network" "vpc" {
 resource "google_compute_subnetwork" "subnetwork" {
   name          = "tech-challenge-subnetwork"
   ip_cidr_range = "10.0.1.0/24"
-  region        = "us-central1"
+  region        = "us-central1" # Região da sub-rede
   network       = google_compute_network.vpc.id
 }
 
 resource "google_container_cluster" "cluster" {
-  name     = "tech-challenge-pedido" # Alterado para corresponder ao nome desejado
-  location = "us-central1"           # Certifique-se de que a localização esteja correta
+  name     = "tech-challenge-pedido" # Nome do cluster
+  location = "us-central1"           # Região do cluster
 
   remove_default_node_pool = true
   initial_node_count       = 1
